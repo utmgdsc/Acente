3b6c59bff2ce29dd9be295397e85c0d251aed6a8
Ritvik Bhardwaj <43301959+AipioxTechson@users.noreply.github.com>
2021-10-09 01:08:45 -0400

    [Setup] chakra framework (#2)
    * add chakra-ui framework
    
    * add pull request template
    
    * add ci/cd for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from "@chakra-ui/react"
+import { IntlProvider } from 'react-intl';
+import en from './languages/en.json';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Landing from './landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <IntlProvider locale="en" messages = {en}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+      </IntlProvider>
+		</ChakraProvider>
   );
 }
 
