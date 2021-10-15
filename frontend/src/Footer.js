c405474fe2806fdaa67860c766467070fcd4fda4
kavyamehta <59890699+kavyamehta@users.noreply.github.com>
2021-10-14 23:08:03 -0400

    Added copyright to footer, updated placement text for login and added background img

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {
+    Center,
     HStack,
   } from "@chakra-ui/react"
 
@@ -7,8 +8,8 @@ class Footer extends Component {
 
     render() {
         return (
-            <HStack backgroundColor="#9AE6B4" height="7vh" >
-                
+            <HStack backgroundColor="#9AE6B4" height="7vh" justifyContent="center">
+                <Center fontSize="sm">Copyright &copy; Acenté 2021 | All rights reserved </Center>
             </HStack>
         )
     }
--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -16,24 +16,24 @@ class Form extends Component {
 
     render() {
         return (
-
-                <VStack height="86vh" position="relative" justifyContent="center">
-                    <Flex direction="column" position="relative" justifyContent="center">
-                        <Center fontSize="5xl" mb="10px">{this.props.value}</Center>
-                        <FormControl id="username">
-                            <FormLabel mb="1px" >Username</FormLabel>
-                            <Input placeholder="Username" borderWidth="2px"/>
-                        </FormControl>
-                        <FormControl id="email">
-                            <FormLabel mb="1px" mt="8px">Email address</FormLabel>
-                            <Input type="email" borderWidth="2px"/>
-                            <FormHelperText mt="1px">We'll (almost) never share your email.</FormHelperText>
-                        </FormControl>
-                    </Flex>
-                    <Button colorScheme="gray">
-                        Submit
-                    </Button>
-                </VStack>
+            <VStack height="86vh" position="relative" justifyContent="center">
+                <Flex direction="column" position="relative" justifyContent="center">
+                    <Center fontSize="7xl" letterSpacing="10px" fontFamily="mono">ACENTÉ</Center>
+                    <Center fontSize="3xl" color="gray">{this.props.value}</Center>
+                    <FormControl id="username">
+                        <FormLabel mb="1px" >Username</FormLabel>
+                        <Input placeholder="JohnDoe" borderWidth="2px"/>
+                    </FormControl>
+                    <FormControl id="email">
+                        <FormLabel mb="1px" mt="8px" >Email address</FormLabel>
+                        <Input type="email" borderWidth="2px"placeholder="johndoe@fakeemail.com"/>
+                        <FormHelperText mt="1px">We'll (almost) never share your email.</FormHelperText>
+                    </FormControl>
+                </Flex>
+                <Button colorScheme="gray">
+                    Submit
+                </Button>
+            </VStack>
         )
     }
 }
