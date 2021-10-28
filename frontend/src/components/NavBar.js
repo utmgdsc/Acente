b680e55566959bf7bce5265ba3ce0c03baf5e101
kavyamehta <59890699+kavyamehta@users.noreply.github.com>
2021-10-28 14:10:13 -0400

    Feat/dashboard/ticket 6 (#6)
    * created login form
    
    * fixed typo
    
    * added signup page
    
    * removed redundant containers
    
    * minor edit
    
    * Finished User Story 3
    
    * Added Acente Icon to repo
    
    * Added copyright to footer, updated placement text for login and added background img
    
    * add remaining changes
    
    * add form tests
    
    * Created dashboard page, Added components to dashboard, and sample text
    
    * Finished User story 6
    
    * fix tests
    
    Co-authored-by: Kewpie <72084927+q-kewpie-q@users.noreply.github.com>
    Co-authored-by: Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
    Co-authored-by: Ritvik Bhardwaj <43301959+AipioxTechson@users.noreply.github.com>

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
 import {
-    Button, HStack, IconButton, Img
+    Button, 
+    HStack, 
+    IconButton, 
+    Img, 
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
   } from "@chakra-ui/react"
 
+import { 
+    HamburgerIcon,
+} from '@chakra-ui/icons'
 
-const NavBar = () => {
+import {GiNotebook} from "react-icons/gi"
+import {CgProfile} from "react-icons/cg"
+import {BiLogOut} from "react-icons/bi"
+import {HiMicrophone} from "react-icons/hi"
+
+
+const NavBar = ({type}) => {
     let history = useHistory();
 
     const handleSignUpClick = () => {  
@@ -19,23 +36,66 @@ const NavBar = () => {
     const handleLoginClick = () => {
         history.push('/login')
     }
-    
-    return (
-        <HStack spacing="20px" backgroundColor="#9AE6B4" height="7vh" paddingRight="20px" paddingLeft="20px" width="100%">
-            <HStack justifyContent="flex-start" width="50%">
-                <IconButton colorScheme="green" onClick={handleMainPageClick} aria-label="Acente Logo" backgroundColor="#9AE6B4" icon={<Img maxWidth="100%" maxHeight="100%" src="AcenteIcon.png"/>}/>
-            </HStack>
+        
+    if (type === "loginMenu") {
+        return (
+            <HStack spacing="20px" backgroundColor="#9AE6B4" height="7vh" paddingRight="20px" paddingLeft="20px" width="100%">
+                <HStack justifyContent="flex-start" width="50%">
+                    <IconButton colorScheme="green" onClick={handleMainPageClick} aria-label="Acente Logo" backgroundColor="#9AE6B4" icon={<Img maxWidth="100%" maxHeight="100%" src="AcenteIcon.png"/>}/>
+                </HStack>
 
-            <HStack justifyContent="flex-end" width="50%">
-                <Button colorScheme="green" variant="outline" onClick={handleLoginClick}>
-                    Login
-                </Button>
-                <Button colorScheme="green" variant="outline" onClick={handleSignUpClick}>
-                    Sign Up
-                </Button>
+                <HStack justifyContent="flex-end" width="50%">
+                    <Button colorScheme="green" variant="outline" onClick={handleLoginClick}>
+                        Login
+                    </Button>
+                    <Button colorScheme="green" variant="outline" onClick={handleSignUpClick}>
+                        Sign Up
+                    </Button>
+                </HStack>
             </HStack>
-        </HStack>
-    )
+        )
+    }
+    else {
+        return (
+            <HStack spacing="20px" backgroundColor="#9AE6B4" height="7vh" paddingRight="20px" paddingLeft="20px" width="100%">
+                <HStack justifyContent="flex-start" width="50%">
+                    <IconButton colorScheme="green" onClick={handleMainPageClick} aria-label="Acente Logo" backgroundColor="#9AE6B4" icon={<Img maxWidth="100%" maxHeight="100%" src="AcenteIcon.png"/>}/>
+                </HStack>
+
+                <HStack justifyContent="flex-end" width="50%">
+                    <Menu>
+                        <MenuButton
+                            as={IconButton}
+                            aria-label="Options"
+                            icon={<HamburgerIcon />}
+                            variant="outline"
+                        />
+                        <MenuList minHeight="100vh" minWidth="35vh" right="-60px" position="absolute" top="-57px" backgroundColor="#2D3748">
+                            <MenuItem minHeight="13vh" icon={<CgProfile />} fontSize="2xl" color="white" _focus={{backgroundColor:"#4A5568"}}>
+                                Profile
+                            </MenuItem>
+                            <MenuDivider/>
+                            <MenuItem minHeight="13vh" icon={<HiMicrophone />} fontSize="2xl" color="white" _focus={{backgroundColor:"#4A5568"}}>
+                            Practice
+                            </MenuItem>
+                            <MenuDivider/>
+                            <MenuItem minHeight="13vh" icon={<GiNotebook />} fontSize="2xl" color="white" _focus={{backgroundColor:"#4A5568"}}>
+                            Sandbox
+                            </MenuItem>
+                            <MenuDivider/>
+                            <MenuItem minHeight="13vh" icon={<BiLogOut />} fontSize="2xl" color="white" _focus={{backgroundColor:"#4A5568"}}>
+                            Logout
+                            </MenuItem>
+                            <MenuDivider/>
+                            <MenuItem fontSize="180px" color="white" _focus={{backgroundColor:"#2D3748"}} paddingLeft="75px">
+                            &#9651;
+                            </MenuItem>
+                        </MenuList>
+                    </Menu>
+                </HStack>
+            </HStack>
+        )
+    }
 }
 
 export default NavBar;
