474f31418fcf224c4e5905493789fc19f98fd871
Ritvik Bhardwaj <43301959+AipioxTechson@users.noreply.github.com>
2021-12-05 17:07:42 -0500

    add minion translation (#31)
    * add minion translation
    
    * add spanish translations
    
    * change manifest json

--- frontend/src/i18n/languages/index.js
+++ frontend/src/i18n/languages/index.js
@@ -1,5 +1,25 @@
 import en from './en.json';
 import fr from './fr.json';
 import ch from './ch.json';
+import mn from './mn.json';
+import es from './es.json';
 
-export default { en, fr, ch };
+
+export const AppLanguage = {
+  English: 'en',
+  Français: 'fr',
+  繁體中文: 'ch',
+  Minion: 'mn',
+  Spanish: 'es'
+};
+
+export const Mapping = {
+  'en': 'English',
+  'fr': 'Français',
+  'ch': '繁體中文',
+  'mn': 'Minion',
+  'es': 'Spanish'
+}
+
+
+export default { en, fr, ch, mn, es };
