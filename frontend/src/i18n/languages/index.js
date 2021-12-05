d842b68998cc41438f191d3fb35eb210ef53b31e
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-12-05 16:24:21 -0500

    add spanish translations

--- frontend/src/i18n/languages/index.js
+++ frontend/src/i18n/languages/index.js
@@ -2,21 +2,24 @@ import en from './en.json';
 import fr from './fr.json';
 import ch from './ch.json';
 import mn from './mn.json';
+import es from './es.json';
 
 
 export const AppLanguage = {
   English: 'en',
   Français: 'fr',
   繁體中文: 'ch',
-  Minion: 'mn'
+  Minion: 'mn',
+  Spanish: 'es'
 };
 
 export const Mapping = {
   'en': 'English',
   'fr': 'Français',
   'ch': '繁體中文',
-  'mn': 'Minion'
+  'mn': 'Minion',
+  'es': 'Spanish'
 }
 
 
-export default { en, fr, ch, mn };
+export default { en, fr, ch, mn, es };
